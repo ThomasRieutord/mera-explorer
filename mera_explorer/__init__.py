5bd9efd7c90e7d0d4c3f5d1e31cf7b56c3601b2e
Thomas Rieutord <thomas.rieutord@met.ie>
2024-07-31 10:15:57 +0000

    Store paths on local machine in a TXT file untracked

--- mera_explorer/__init__.py
+++ mera_explorer/__init__.py
@@ -7,8 +7,6 @@ https://github.com/ThomasRieutord/mera-explorer
 import os
 
 PACKAGE_DIRECTORY = os.path.split(__path__[0])[0]
-MERAROOTDIR = "/data/trieutord/MERA/grib-all"
-MERACLIMDIR = "/data/trieutord/MERA/meraclim"
 
 NEURALLAM_VARIABLES = [  # Order matters
     "air_pressure_at_surface_level",
@@ -30,6 +28,17 @@ NEURALLAM_VARIABLES = [  # Order matters
     "geopotential_at_1000_hPa",
 ]
 
+### Paths to MERA data on this machine
+# [... mera-explorer]$ cat local/paths.txt
+# MERAROOTDIR = "/data/trieutord/MERA/grib-all" # Parent directory of all MERA GRIB files
+# MERACLIMDIR = "/data/trieutord/MERA/meraclim" # Directory where are stored climatology data (in particular the m05.grib)
+
+with open(os.path.join(PACKAGE_DIRECTORY, "local", "paths.txt"), "r") as f:
+    for l in f.readlines():
+        if "MERAROOTDIR" in l:
+            MERAROOTDIR = l.split('"')[1]
+        if "MERACLIMDIR" in l:
+            MERACLIMDIR = l.split('"')[1]
 
 with open(os.path.join(PACKAGE_DIRECTORY, "setup.py"), "r") as f:
     for l in f.readlines():
--- tests/import_tests.py
+++ tests/import_tests.py
@@ -7,5 +7,10 @@ Program to test import of the package.
 import mera_explorer
 from mera_explorer import gribs
 from mera_explorer import transfer
-from mera_explorer.data import neurallam
+from mera_explorer import forecasts
 
+from mera_explorer import PACKAGE_DIRECTORY, MERAROOTDIR, MERACLIMDIR
+
+print(f"Package {mera_explorer} successfully imported from {PACKAGE_DIRECTORY}")
+print(f"GRIB files are expected to be in {MERAROOTDIR}")
+print(f"Climatology file (m05.grib) is expected to be in {MERACLIMDIR}")
